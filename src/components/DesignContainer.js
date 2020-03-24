947d4a2afa06ddee1ef82995e3e0f16646af8d8d
Cameron Flowers <cameron.flowers13@gmail.com>
2020-03-24 17:54:05 -0500

    added DesignContainer.js

---
+++ src/components/DesignContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class DesignContainer extends React.Component {
+
+
+    render(){
+        return(
+            <div>
+                Design Container
+            </div>
+        )
+    }
+
+
+}
+
+export default DesignContainer
