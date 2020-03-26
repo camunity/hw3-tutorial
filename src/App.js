010a84ce2c24a950c1c429a8d1c485007b1614c3
Cameron Flowers <cameron.flowers13@gmail.com>
2020-03-25 22:53:25 -0500

    created a new dummy component to show off git principles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DesignContainer from './components/DesignContainer.js';
 
 function App() {
   return (
       <div>
-          THIS IS MY HW3
+          <DesignContainer />
       </div>
   );
 }
--- src/components/DesignContainer.js
+++ src/components/DesignContainer.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import '../designpad.css';
 
 class DesignContainer extends React.Component {
 
+    constructor(props){
+        super(props)
+
+        this.state = {
+            color: 'orange'
+        }
+    }
 
     render(){
         return(
-            <div>
-                Design Container
+            <div class="pad"> 
             </div>
         )
     }
