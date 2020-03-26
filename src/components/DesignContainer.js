50fdf060f877c152c10bcef38a2ff6da7b40b052
Cameron Flowers <cameron.flowers13@gmail.com>
2020-03-26 17:52:51 -0500

    live coding on dynamic react styles with the homies in the class

--- src/components/DesignContainer.js
+++ src/components/DesignContainer.js
@@ -8,28 +8,34 @@ class DesignContainer extends React.Component {
 
         this.state = {
             color: 'grey',
-            input: 1
+            input: 1,
+            showPads: true,
+            padStyle: 'blackpad'
         }
     }
 
     changeBackground = (event) => {
         if(this.state.input == 1){
-            this.setState({ color: 'blue', input: 0 })
+            this.setState({ padStyle: 'blackpad', input: 0 })
         } else{
-            this.setState({ color: 'red', input: 1 })
+            this.setState({ padStyle: 'orangepad', input: 1 })
         }
     }
 
     render(){
-        
-        return(
-            <div>
-                <button onClick={()=>this.changeBackground()}> Change Color </button>
-                <div className="blackpad"> </div>
-                <div className="orangepad"> </div>
-                <div className="pad" style={{ background: this.state.color }}></div>
-            </div>
-        )
+
+        if(this.state.showPads == false){
+            return (<div>There's no input! :( </div>)
+        } else {
+            return(
+                <div>
+                    <button onClick={()=>this.changeBackground()}> Change Color </button>
+                    <div className="blackpad"> </div>
+                    <div className="orangepad"> </div>
+                    <div className={this.state.padStyle}></div>
+                </div>
+            )
+        }
     }
 
 }
