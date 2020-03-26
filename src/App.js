2e09bf9cf92246fa379b4141cca17088c1c331d8
Cameron Flowers <cameron.flowers13@gmail.com>
2020-03-25 23:01:28 -0500

    tested NewComponent and expected output

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import DesignContainer from './components/DesignContainer.js';
+import NewComponent from './components/NewComponent';
 
 function App() {
   return (
       <div>
-          <DesignContainer />
+          <NewComponent />
       </div>
   );
 }
